fd10c90bf358a958f90fe6f56227dabb42995452
tijmenvanegmond <nemjit.12@gmail.com>
2017-06-20 21:17:26 +0200

    init

--- GenericWebshop/Global.asax.cs
+++ GenericWebshop/Global.asax.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using Newtonsoft.Json;
 
 namespace GenericWebshop
 {
@@ -14,6 +16,9 @@ namespace GenericWebshop
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
