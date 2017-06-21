13c6571f28c6f1c8f2da1888903023a51e7539da
tijmenvanegmond <nemjit.12@gmail.com>
2017-06-21 23:27:37 +0200

    Added basic property sorting troug the items api

--- GenericWebshop/Controllers/HomeController.cs
+++ GenericWebshop/Controllers/HomeController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GenericWebshop.Models;
 
 namespace GenericWebshop.Controllers
 {
     public class HomeController : Controller
     {
+        private webshopEntities db = new webshopEntities();
+
         public ActionResult Index()
         {
+            var categories = db.Categories.ToList().Select(x => new KeyValuePair<int, string>(x.Id, x.Title)).ToList();
+            var properties = typeof(Item).GetProperties().Select(p => p.Name).ToList();
+            ViewData.Add("categories", categories);
+            ViewData.Add("properties", properties);
             return View();
         }
 
